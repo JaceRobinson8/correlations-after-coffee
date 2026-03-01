df4756638eff8a772d1f6cba8d728188401f7756
jacerobinson21@gmail.com <12394543+JaceRobinson8@users.noreply.github.com>
2026-03-01 11:09:19 -0500

    getting the initial homepage view setup.

--- my_app/correlations/views.py
+++ my_app/correlations/views.py
@@ -1,6 +1,25 @@
-from django.http import HttpResponse
+from django.views import generic
 
 
-# Create your views here.
-def index(request) -> HttpResponse:
-    return HttpResponse("Hello World")
+class IndexView(generic.ListView):
+    template_name = "correlations/index.html"
+    context_object_name = "latest_entries"
+
+    def get_queryset(self) -> list:
+        return ["hello"]
+
+
+# # Create your views here.
+# def index(request) -> HttpResponse:
+#     return HttpResponse("Hello World")
+
+
+# class IndexView(generic.ListView):
+#     template_name = "polls/index.html"
+#     context_object_name = "latest_question_list"
+
+#     def get_queryset(self):
+#         """Return the last five published questions."""
+#         return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+#             "-pub_date"
+#         )[:5]
