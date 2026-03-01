e3c8153e459c7642b2148b168479045d77d93368
jacerobinson21@gmail.com <12394543+JaceRobinson8@users.noreply.github.com>
2026-03-01 12:02:38 -0500

    cleaning up the model,

--- my_app/correlations/models.py
+++ my_app/correlations/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 
 class Post(models.Model):
-    title: models.CharField(max_length=100)
-    description: models.CharField(max_length=500)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
     slug = models.SlugField(unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     published_at = models.DateTimeField("date published")
-    content: models.TextField()
+    content = models.TextField(default="")
 
     def __str__(self):
         return self.title
--- my_app/correlations/views.py
+++ my_app/correlations/views.py
@@ -5,9 +5,18 @@ from django.views import generic
 from .models import Post
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
-    return render(request, "blog/post_list.html", {"posts": posts})
+class IndexView(generic.ListView):
+    template_name = "correlations/index.html"
+    context_object_name = "latest_entries"
+
+    def get_queryset(self) -> list:
+        return Post.objects.all()
+        # return Post.objects.filter(published_at__lte=timezone.now())
+
+
+# def post_list(request):
+#     posts = Post.objects.filter(published_at__lte=timezone.now())
+#     return render(request, "blog/post_list.html", {"posts": posts})
 
 
 class DetailView(generic.DetailView):
